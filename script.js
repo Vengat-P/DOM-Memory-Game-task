4521a651cbc5175d9b37e78a002579185c0dc6a6
Vengat-P <cronyvengat33@gmail.com>
2025-04-10 07:15:30 +0530

    base styling completed

---
+++ script.js
@@ -0,0 +1,19 @@
+const heading = document.getElementById("header");
+const restart = document.getElementById("restart");
+
+
+//! handleload function
+
+const handlestart = () => {
+  let set1 = [
+    
+  ];
+  const final = [...set1, ...set1];
+  const suffule = Math.floor(Math.random() * (final.length - 1));
+}
+//! handle restart function
+
+//! card flip function
+
+
+//! check pairs
